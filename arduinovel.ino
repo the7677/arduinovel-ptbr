093616841387e3f98d451937fb3c1879bf3b0f7c
the7677 <pedro.brito.7677@gmail.com>
2025-03-13 22:51:07 -0300

    sprite buffer

--- arduinovel.ino
+++ arduinovel.ino
@@ -1,23 +1,29 @@
 // Libs
 #include <U8g2lib.h>
 #include <string.h>
+#include <stdlib.h>
 
-// Inner Files
+// Arquivos internos
 #include "sprites.h"
 #include "timer.h"
 #include "dialog_lines.h"
 
-// Controls
+// Controles
 #define DOWN(BTN) digitalRead(BTN)
 #define A      2
 #define DLEFT  3
 #define DRIGHT 4
 
-// Sound
-#define BUZZER 6
-#define MAY_VOICE 440*4
+// Som
+#define BUZZER_1 6
 
-// Fonts
+#define MAY_VOICE 440 * 4 + (rand() % 76)
+#define MAY_VOICE_MS 10
+#define MAY_VOICE_PAUSE 2
+
+#define PLAY_VOICE(BUZZER, VOICE, MS, PAUSE) tone(BUZZER, VOICE); delay(MS); noTone(BUZZER); delay(PAUSE)
+
+// Texto
 #define FONT_1 u8g2_font_5x7_mr
 #define FONT_2 u8g2_font_spleen5x8_mf
 #define LINE_START_X 50
@@ -25,20 +31,32 @@
 #define LETTER_W     5
 #define LETTER_H     8
 
+#define CURSOR_CHAR dbuff[dcursor]
+
+#define UPDATE_DLINE strcpy_P(dbuff, (char*)pgm_read_word(&(dlines[dline_i])))
+#define DLINE_LEN strlen(dbuff)
+
+// Sprites
+#define SPRITE sbuff
+
+#define MAY_TO_BUFFER memcpy_P(sbuff, MAY_BITS, (MAY_W * MAY_H)/8)
+#define SPLASH_TO_BUFFER memcpy_P(sbuff, SPLASH_BITS, (SPLASH_W * SPLASH_H)/8)
+
 // Display
 U8G2_SH1106_128X64_NONAME_F_HW_I2C u8g2(U8G2_R0, /* reset=*/ U8X8_PIN_NONE);
 
-// Dialog vars
+// Diálogo
 size_t dcursor = 0;
-int dcursor_x  = LINE_START_X;
-int dcursor_y  = LINE_START_Y;
+int8_t dcursor_x  = LINE_START_X;
+int8_t dcursor_y  = LINE_START_Y;
 
 size_t dline_i = 0;
 
 inline void drawGame() {
   u8g2.setDrawColor(1);
 
-  u8g2.drawXBM(0, 0, MAY_W, MAY_H, MAY_BITS);
+  MAY_TO_BUFFER;
+  u8g2.drawXBM(0, 0, MAY_W, MAY_H, SPRITE);
 
   u8g2.drawLine(43, 31, 46, 25);
   u8g2.drawLine(43, 32, 46, 39);
@@ -55,49 +73,38 @@ inline void drawGame() {
   u8g2.setDrawColor(1);
 }
 
-
-
-void drawDialog() {
-  UPDATE_LINE_BUFFER;
-  const char c = buff[dcursor];
-
-  switch (c) {
+inline void drawDialog() {
+  switch (CURSOR_CHAR) {
   case '\n':
     dcursor_x = LINE_START_X;
     dcursor_y += LETTER_H;
     break;
   default:
-    u8g2.drawGlyph(dcursor_x, dcursor_y, c);
+    u8g2.drawGlyph(dcursor_x, dcursor_y, CURSOR_CHAR);
     dcursor_x += LETTER_W;
     break;
   }
 }
 
-void updateDialog() {
-  UPDATE_LINE_BUFFER;
-  
+inline void updateDialog() {
   // Input
-  if (DOWN(A)) {
-    // Próximo diálogo
-    if (dcursor == strlen(buff)) {
-      clearDialog();
-      dline_i++;
-      dcursor = 0;
-      dcursor_x = LINE_START_X;
-      dcursor_y = LINE_START_Y;
+  // Próximo diálogo
+  if (DOWN(A) && dcursor == DLINE_LEN) {
+    clearDialog();
+    dline_i++;
+    UPDATE_DLINE;
+    dcursor = 0;
+    dcursor_x = LINE_START_X;
+    dcursor_y = LINE_START_Y;
 
-      return; // Não atualiza
-    // Ir ao fim do diálogo
-    }
+    return;
+  // Ir ao fim do diálogo
   }
 
   // Update
-  if (dcursor < strlen(buff)) {
+  if (dcursor < DLINE_LEN) {
     dcursor++;
-    tone(BUZZER, MAY_VOICE);
-    delay(10);
-    noTone(BUZZER);
-    delay(2);
+    PLAY_VOICE(BUZZER_1, MAY_VOICE, MAY_VOICE_MS, MAY_VOICE_PAUSE);
   }
 }
 
@@ -115,6 +122,8 @@ void setup() {
   u8g2.begin();
   u8g2.setContrast(0);
   u8g2.setFont(FONT_1);
+
+  UPDATE_DLINE;
 
   drawGame();
 }
--- dialog_lines.h
+++ dialog_lines.h
@@ -3,27 +3,27 @@
 #ifndef DIALOG_LINES_H
 #define DIALOG_LINES_H
 
-#define UPDATE_LINE_BUFFER (strcpy_P(buff, (char*)pgm_read_word(&(dlines[dline_i]))))
-
 #define MAX_STRLENGTH 64
 
-const char LINE_0[] PROGMEM = "Ola, bom dia!";
-const char LINE_1[] PROGMEM = "Tudo bem? Como\nvoce esta?";
-const char LINE_2[] PROGMEM = "Ja ouvi falar\num pouco sobre\nvoce.";
-const char LINE_3[] PROGMEM = "Fodidos ameri-\ncanos...";
-const char LINE_4[] PROGMEM = "Nao incluiram\nacento na tabe-\nla ascii.";
-const char LINE_5[] PROGMEM = "Por favor, nao\npule para o\nproximo dia-\nlogo.";
-const char LINE_6[] PROGMEM = "Esta querendo\nlevar um\nSIGSEGV na sua\ncara?";
-const char LINE_7[] PROGMEM = "/@#6 !!(0  ((\"\nł?°ŋħ“«»©«";
-const char LINE_8[] PROGMEM = "Brincadeira!\nNenhum acesso e\ninvalido aqui\nno ATMEGA328P.";
-const char LINE_9[] PROGMEM = "O maximo que\npode acontecer\ne um cheirinho\nde queimado.";
+// Buffer das strings na memória flash representadas na RAM
+char dbuff[MAX_STRLENGTH];
+
+//                                                                                             v-- Limite, exceto por caracteres especiais com '\'
+const char LINE_00[] PROGMEM = "Ola, bom dia!";
+const char LINE_01[] PROGMEM = "Tudo bem? Como\nvoce esta?";
+const char LINE_02[] PROGMEM = "Ja ouvi falar\num pouco sobre\nvoce.";
+const char LINE_03[] PROGMEM = "Malditos ameri-\ncanos...";
+const char LINE_04[] PROGMEM = "Nao incluiram\nacento na tabe-\nla ascii.";
+const char LINE_05[] PROGMEM = "Por favor, nao\npule para o\nproximo dia-\nlogo.";
+const char LINE_06[] PROGMEM = "Esta querendo\nlevar um\nSIGSEGV na sua\ncara?";
+const char LINE_07[] PROGMEM = "/@#6 !!(0  ((\"\n  ?°    .     ";
+const char LINE_08[] PROGMEM = "Brincadeira!\nNenhum acesso e\ninvalido aqui\nno ATMEGA328P.";
+const char LINE_09[] PROGMEM = "O maximo que\npode acontecer\ne um cheirinho\nde queimado.";
+const char LINE_10[] PROGMEM = "void\\\n(*reset)() = 0;\nreset();";
 
 const char* const dlines[] PROGMEM = {
-    LINE_0, LINE_1, LINE_2, LINE_3, LINE_4, LINE_5, LINE_6, LINE_7, LINE_8,
-    LINE_9,
+    LINE_00, LINE_01, LINE_02, LINE_03, LINE_04, LINE_05, LINE_06, LINE_07, LINE_08,
+    LINE_09, LINE_10
 };
-
-// Buffer das strings na memória flash representadas na RAM
-char buff[MAX_STRLENGTH];
 
 #endif 
