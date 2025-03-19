e65e3cb9b8018dc4c262d744ef8e8375281dfbe0
the7677 <pedro.brito.7677@gmail.com>
2025-03-18 23:24:46 -0300

    codificação ISO 8859-1

--- dialog_lines.h
+++ dialog_lines.h
@@ -4,20 +4,20 @@
 #define MAX_STRLENGTH 64
 
 // Buffer das strings na RAM
-extern char dbuff[MAX_STRLENGTH];
+extern unsigned char dbuff[MAX_STRLENGTH];
 
-extern const char LINE_00[] PROGMEM;
-extern const char LINE_01[] PROGMEM;
-extern const char LINE_02[] PROGMEM;
-extern const char LINE_03[] PROGMEM;
-extern const char LINE_04[] PROGMEM;
-extern const char LINE_05[] PROGMEM;
-extern const char LINE_06[] PROGMEM;
-extern const char LINE_07[] PROGMEM;
-extern const char LINE_08[] PROGMEM;
-extern const char LINE_09[] PROGMEM;
-extern const char LINE_10[] PROGMEM;
+extern const unsigned char LINE_00[] PROGMEM;
+extern const unsigned char LINE_01[] PROGMEM;
+extern const unsigned char LINE_02[] PROGMEM;
+extern const unsigned char LINE_03[] PROGMEM;
+extern const unsigned char LINE_04[] PROGMEM;
+extern const unsigned char LINE_05[] PROGMEM;
+extern const unsigned char LINE_06[] PROGMEM;
+extern const unsigned char LINE_07[] PROGMEM;
+extern const unsigned char LINE_08[] PROGMEM;
+extern const unsigned char LINE_09[] PROGMEM;
+extern const unsigned char LINE_10[] PROGMEM;
 
-extern const char* const dlines[] PROGMEM;
+extern const unsigned char* const dlines[] PROGMEM;
 
 #endif
--- dialog_lines.ino
+++ dialog_lines.ino
@@ -1,24 +1,29 @@
 #include <avr/pgmspace.h>
 
-// Buffer das strings na memÃ³ria flash representadas na RAM
-char dbuff[MAX_STRLENGTH];
+// Importante: A codificação desse arquivo precisa estar em ISO 8859-1 para uma leitura correta
+
+// Buffer das strings na memória flash representadas na RAM
+unsigned char dbuff[MAX_STRLENGTH];
 
 // '*' inverte a cor (negrito). Usar '**' para ignorar.
-// // O modo negrito sempre Ã© interrompido ao mudar de linha, e '*' continua ocupando 1 caractere na memÃ³ria
+// // O modo negrito interrompe ao mudar de linha, e '*' continua ocupando 1 caractere na memória
 //                                                                                             v-- Limite, exceto por caracteres especiais com '\'
-const char LINE_00[] PROGMEM = "*Ola*, **bom dia!**";
-const char LINE_01[] PROGMEM = "Tudo bem? Como\nvoce esta?";
-const char LINE_02[] PROGMEM = "Ja ouvi falar\num pouco sobre\nvoce.";
-const char LINE_03[] PROGMEM = "Malditos ameri-\ncanos...";
-const char LINE_04[] PROGMEM = "Nao incluiram\nacento na tabe-\nla ascii.";
-const char LINE_05[] PROGMEM = "Por favor, nao\npule para o\nproximo dialo-\ngo.";
-const char LINE_06[] PROGMEM = "Esta querendo\nlevar um\nSIGSEGV na sua\ncara?";
-const char LINE_07[] PROGMEM = "/@#6 !!(0  ((\"\n  ?Â° * *  .     ";
-const char LINE_08[] PROGMEM = "Brincadeira!\nNenhum acesso e\ninvalido aqui\nno ATMEGA328P.";
-const char LINE_09[] PROGMEM = "O maximo que\npode acontecer\ne um cheirinho\nde queimado.";
-const char LINE_10[] PROGMEM = "void\\\n(**reset)() = 0;\nreset();";
+const unsigned char LINE_00[] PROGMEM = "Ola, bom dia!";
+const unsigned char LINE_01[] PROGMEM = "Tudo bem? Como\nvoce esta?";
+const unsigned char LINE_02[] PROGMEM = "Malditos ameri-\ncanos...";
+const unsigned char LINE_03[] PROGMEM = "Nao incluiram\nacento na tabe-\nla ascii.";
+const unsigned char LINE_04[] PROGMEM = "Mas olha só!\nAgora há com-\npatibilidade!!";
+const unsigned char LINE_05[] PROGMEM = "Só precisei\ntrocar de UTF8\npara ISO\n8859-1.";
+const unsigned char LINE_06[] PROGMEM = "...";
+const unsigned char LINE_07[] PROGMEM = "Por favor, não\npule para o\npróximo diálo-\ngo.";
+const unsigned char LINE_08[] PROGMEM = "O espaço na me-\nmória está\nacabando...";
+const unsigned char LINE_09[] PROGMEM = "Está querendo\nlevar um\nSIGSEGV na sua\ncara?";
+const unsigned char LINE_10[] PROGMEM = "/@#6 !!(0  ((\"\n  ?Â° * *  . ?   ";
+const unsigned char LINE_11[] PROGMEM = "Brincadeira!\nNenhum acesso é\ninvalido aqui\nno ATMEGA328P.";
+const unsigned char LINE_12[] PROGMEM = "O máximo que\npode acontecer\né um cheirinho\nde queimado.";
+const unsigned char LINE_13[] PROGMEM = "void\\\n(**reset)() = 0;\nreset();";
 
-const char* const dlines[] PROGMEM = {
+const unsigned char* const dlines[] PROGMEM = {
     LINE_00, LINE_01, LINE_02, LINE_03, LINE_04, LINE_05, LINE_06, LINE_07, LINE_08,
-    LINE_09, LINE_10
+    LINE_09, LINE_10, LINE_11, LINE_12, LINE_13
 };
